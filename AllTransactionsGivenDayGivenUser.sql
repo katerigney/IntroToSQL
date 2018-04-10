8194ec4f8bef419a15d2aef78b3cf3cdd82d615c
Rigney <krigney@proforma.com>
2018-04-10 18:55:37 -0400

    DONE!

---
+++ AllTransactionsGivenDayGivenUser.sql
@@ -0,0 +1,12 @@
+USE [BankTransactions]
+GO
+
+SELECT [ID]
+      ,[Timestamp]
+      ,[Action]
+      ,[AccountNumber]
+      ,[AmountChanged]
+      ,[NewAmount]
+  FROM [dbo].[Transactions]
+ WHERE Timestamp < '2018-03-14 23:59:00' and Timestamp > '2018-03-13 23:59:00' and AccountNumber = 576734
+ GO
