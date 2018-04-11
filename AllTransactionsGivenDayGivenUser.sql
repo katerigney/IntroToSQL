f6feee9765c776b299a84127c6279942e887a7f7
Rigney <krigney@proforma.com>
2018-04-11 09:56:19 -0400

    realized that I was returning all the information for 'All Transactions Given User/Given Day' not the count. Lecture made me realize my mistake.

--- AllTransactionsGivenDayGivenUser.sql
+++ AllTransactionsGivenDayGivenUser.sql
@@ -1,12 +1,7 @@
 USE [BankTransactions]
 GO
 
-SELECT [ID]
-      ,[Timestamp]
-      ,[Action]
-      ,[AccountNumber]
-      ,[AmountChanged]
-      ,[NewAmount]
+SELECT Count (*)
   FROM [dbo].[Transactions]
  WHERE Timestamp < '2018-03-14 23:59:00' and Timestamp > '2018-03-13 23:59:00' and AccountNumber = 576734
  GO
